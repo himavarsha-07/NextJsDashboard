a776d660e8b0ea142b39ff5a96fd653f8f6850df
Himavarsha <himavarsha.sagineti@technoidentity.com>
2023-11-15 12:20:21 +0530

    Image alt for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -33,17 +33,17 @@ export default function Page() {
           {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
             width={1000}
             height={760}
             className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
             src="/hero-mobile.png"
+            alt="Screenshot of the dashboard project showing mobile version"
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Screenshot of the dashboard project showing mobile version"
           />
         </div>
       </div>
